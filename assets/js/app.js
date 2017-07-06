2b14adc3e23c8ff5502a3fea10cb1dd9cc63ee74
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-07-06 15:06:41 -0300

    Vue Hello World

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,10 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+new Vue({
+  el: '#vue-playground',
+  data: {
+    message: 'Hello to Vue World!'
+  }
+});
